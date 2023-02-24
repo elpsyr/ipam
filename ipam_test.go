23dc39649fe0be69122c5e7f06011521f11b6749
crui <crui@relaper.com>
2023-02-24 15:56:39 +0800

    add ipam concurrency test

--- ipam_test.go
+++ ipam_test.go
@@ -1,6 +1,10 @@
 package ipam
 
-import "testing"
+import (
+	"fmt"
+	"sync"
+	"testing"
+)
 
 // 目前测试数据需要手动删除：
 // ETCDCTL_API=3 etcdctl --endpoints https://172.16.0.124:2379 --cacert /etc/kubernetes/pki/etcd/ca.crt --cert /etc/kubernetes/pki/etcd/healthcheck-client.crt --key /etc/kubernetes/pki/etcd/healthcheck-client.key del  /testcni/ipam --prefix
@@ -17,4 +21,35 @@ func TestNew(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
+}
+
+// 测试多个客户端同时从IP池获取IP
+func TestConcurrencyGetIP(t *testing.T) {
+
+	// 前置条件 创建IP池
+	ipam, err := New(Config{
+		Subnet: "10.244.0.0/16",
+		conn: ConnectionInfo{
+			EtcdEndpoints:  "https://172.16.0.124:2379",
+			EtcdCertFile:   "D:\\Project\\elpsyr\\ipam\\test\\tls\\healthcheck-client.crt",
+			EtcdKeyFile:    "D:\\Project\\elpsyr\\ipam\\test\\tls\\healthcheck-client.key",
+			EtcdCACertFile: "D:\\Project\\elpsyr\\ipam\\test\\tls\\ca.crt",
+		},
+	})
+	if err != nil {
+		t.Error(err)
+	}
+
+	var wg sync.WaitGroup
+	// mock 多客户端获取IP
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		poolPath := getEtcdPathWithPrefix("/" + ipam.Subnet + "/" + ipam.MaskSegment + "/" + "pool")
+		go func() {
+			defer wg.Done()
+			ip, _ := ipam.getIpFromPools(poolPath)
+			fmt.Println(ip)
+		}()
+	}
+	wg.Wait()
 }
