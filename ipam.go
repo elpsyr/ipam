6e436fab52376f11325f6beb2c98687221fd1a26
he11c <hellccqcq@gmail.com>
2023-02-25 14:54:58 +0800

    fix ipam nextUnusedIP when no ip used

--- ipam.go
+++ ipam.go
@@ -124,12 +124,18 @@ func (is *IpAddressManagement) nextUnusedIP() (string, error) {
 	if err != nil {
 		return "", err
 	}
-	allUsedIPs, err := is.EtcdClient.Get(context.TODO(), getRecordPath(string(currentNodeSubnetNetwork.Kvs[0].Value)))
+	allUsedIPsResp, err := is.EtcdClient.Get(context.TODO(), getRecordPath(string(currentNodeSubnetNetwork.Kvs[0].Value)))
 	if err != nil {
 		return "", err
 	}
+	var allUsedIPs string
+	if len(allUsedIPsResp.Kvs) > 0 {
+		allUsedIPs = string(allUsedIPsResp.Kvs[0].Value)
+	} else {
+		allUsedIPs = ""
+	}
 	ipsMap := map[string]bool{}
-	ips := strings.Split(string(allUsedIPs.Kvs[0].Value), ";")
+	ips := strings.Split(allUsedIPs, ";")
 
 	// 标记该IP已经使用
 	for _, ip := range ips {
